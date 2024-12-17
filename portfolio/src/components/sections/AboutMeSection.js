aa57e4f739dec01b9f1975cdfc87e452ac9f8643
Josh Chen <joshchen2003@gmail.com>
2024-12-16 22:42:38 -0500

    initial about me section

--- portfolio/src/components/sections/AboutMeSection.js
+++ portfolio/src/components/sections/AboutMeSection.js
@@ -3,9 +3,14 @@ import Image from 'next/image'
 
 const AboutMeSection = () => {
     return (
-        <div>
-            AboutMeSection
+        <div className='flex flex-row gap-4'>
             <Image src='/sunglass-squirtle.jpg' width='424' height='320' alt="image of me" />
+            <div>
+                <h1 className="text-xl">About Me</h1>
+                <p className="">
+                    My name is Josh and I am a Senior at Duke studying Computer Science. I started out as a Biology major on the pre-med track, but after taking a few Computer Science classes and coming into closer contact with the injustices rooted in the healthcare system, I decided to make the switch to work on projects that can help people on a larger scale. I enjoy creating tools that seek to solve the little inconveniences in my life.  
+                </p>
+            </div>    
         </div>
     )
 }
